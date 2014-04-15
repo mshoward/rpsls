5c079cd05155fa483d31155924ac694a7a38e57e
Mason Howard <mshoward42@gmail.com>
2014-04-15 15:36:48 -0500

    file

---
+++ rpsls.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+#include <list>
+#include <map>
+
+//using namespace std;
+
+int main()
+{
+	std::map <int, std::string> choices;
+	choices[1] = "rock";
+	choices[2] = "paper";
+	choices[3] = "scissors";
+	choices[4] = "lizard";
+	choices[5] = "spock";
+	
+	
+	
+	return 0;
+}
